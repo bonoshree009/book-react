304ad8a422a1655d4e14e66af30fb28aebab262c
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-10-07 15:52:22 +0600

    add more

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import bookimg from '../../assets/books.jpg'
+
+const Banner = () => {
+    return (
+        <div className='flex  flex-col gap-4 justify-between items-center p-16 w-full bg-[#313d49] rounded md:0 md:flex-row'>
+          <div className='flex-1'>
+            <h1 className=' text-2xl md:text-5xl'>Books to freshen up your bookshelf.</h1>
+             <button className='btn bg-[#1D232A] mt-5'>View The List</button>
+          </div>
+          <div className='flex-1 '><img src={bookimg} alt="" /></div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
-
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider } from "react-router/dom";
+import { router } from './routes/Routes.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+
+
 
 
 createRoot(document.getElementById('root')).render(
